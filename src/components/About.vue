09da737ea3b9598f60bfe18463ff8ddbff360410
Greg Lin <glin@glin.io>
2018-07-26 20:07:48 -0500

    About catsum

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,13 @@
 <template>
-  <h4 :style="{ textAlign: 'center' }">About</h4>
+  <div class="about">
+    <p>Cat ipsum dolor sit amet, bobcat but maine coon. Lynx puma. Panther havana brown. Burmese norwegian forest for kitten grimalkin abyssinian. Manx sphynx, tiger and norwegian forest munchkin mouser yet himalayan. Jaguar. Cougar birman and american bobtail yet tabby tiger. British shorthair himalayan so kitty and bobcat for sphynx but lion. Kitty bombay, british shorthair yet havana brown cheetah so birman, for maine coon. British shorthair havana brown but manx. Kitten singapura so ocicat scottish fold. Scottish fold egyptian mau egyptian mau yet devonshire rex tiger british shorthair. Bobcat donskoy and havana brown ocelot tom, american shorthair. Tomcat. Jaguar maine coon but tom munchkin. Cornish rex ocelot but mouser persian but cheetah grimalkin cornish rex.</p>
+    <p>Devonshire rex thai for cheetah yet leopard, mouser. Turkish angora abyssinian british shorthair tiger ragdoll but norwegian forest so tabby. Kitty. Panther siamese or kitten yet thai but british shorthair cougar. Singapura. Tabby donskoy devonshire rex. Thai maine coon so himalayan ocicat american shorthair for manx. Donskoy munchkin or malkin. Tabby bombay so cheetah savannah jaguar singapura or persian. Birman turkish angora birman for abyssinian and leopard. Cougar burmese. Manx british shorthair devonshire rex but tom siberian, ocelot ragdoll. Russian blue havana brown or kitty singapura, siberian so siberian so himalayan. Birman siamese singapura for kitten, leopard. Cheetah manx leopard. Balinese balinese so devonshire rex. Persian russian blue yet munchkin bengal. Himalayan. Savannah balinese so munchkin sphynx. Singapura maine coon for mouser so persian so maine coon but tomcat. Russian blue. Grimalkin turkish angora. Lion tom tomcat. Cornish rex american shorthair jaguar, but scottish fold tom.</p>
+    <p>Bengal panther. Cheetah ragdoll. Grimalkin ragdoll lion for ocelot turkish angora. Bobcat. Ocicat puma, or maine coon. Norwegian forest norwegian forest thai ocelot.</p>
+  </div>
 </template>
+
+<style scoped>
+.about {
+  padding: 10px 10px 0 10px;
+}
+</style>
