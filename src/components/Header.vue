c8e47902062fd486f0692dd0ffe71e80f3a81017
Greg Lin <glin@glin.io>
2018-07-28 21:56:11 -0500

    Fix sticky navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,23 @@
 <template>
-  <div>
-    <b-navbar toggleable sticky variant="light" type="light" class="header">
-      <b-container>
-        <b-navbar-toggle target="nav-menu-collapse" />
-        <b-navbar-brand :to="'/' + query" class="brand">
-          <img class="logo" src="../assets/neostocks.png"> neostocks
-        </b-navbar-brand>
-        <b-collapse id="nav-menu-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item :to="'/bargain' + query">Bargain</b-nav-item>
-            <b-nav-item :to="'/hot' + query">Hot</b-nav-item>
-            <b-nav-item :to="'/index' + query">Index</b-nav-item>
-          </b-navbar-nav>
+  <b-navbar toggleable sticky variant="light" type="light" class="header">
+    <b-container>
+      <b-navbar-toggle target="nav-menu-collapse" />
+      <b-navbar-brand :to="'/' + query" class="brand">
+        <img class="logo" src="../assets/neostocks.png"> neostocks
+      </b-navbar-brand>
+      <b-collapse id="nav-menu-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item :to="'/bargain' + query">Bargain</b-nav-item>
+          <b-nav-item :to="'/hot' + query">Hot</b-nav-item>
+          <b-nav-item :to="'/index' + query">Index</b-nav-item>
+        </b-navbar-nav>
 
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item to="/about">About</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-container>
-    </b-navbar>
-  </div>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/about">About</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
 </template>
 
 <style scoped>
