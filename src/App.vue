a8a89fef0d83dab987167b842dfd0e3e0bb27833
Greg Lin <glin@glin.io>
2018-07-28 23:32:55 -0500

    Use HTTPS

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@
 </template>
 
 <style>
-@import url('//fonts.googleapis.com/css?family=Open+Sans:400');
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:400');
+
 body {
   background-color: #f7f7f7 !important;
   font-family: 'Open Sans';
