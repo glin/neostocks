78e9a894c6fa13b49e552a757ff6628013c47093
Greg Lin <glin@glin.io>
2018-08-02 23:31:42 -0500

    Add alt text

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container class="text-center">
-    <img src="/static/404.png">
+    <img src="/static/404.png" alt="404, page not found">
     <h4>page not found</h4>
   </b-container>
 </template>
