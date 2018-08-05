c3842ca933e233adcb4098ee4542f57543b61891
Greg Lin <glin@glin.io>
2018-08-04 23:49:24 -0500

    Fill out ticker page

--- src/components/Ticker.vue
+++ src/components/Ticker.vue
@@ -1,84 +1,151 @@
 <template>
   <div>
-    <div class="content">
-      <div class="heading">
-        <a :href="COMPANY_PROFILE_URL + company.id" target="_blank" rel="noopener noreferrer">
-          <img :src="company.logoUrl" class="company-logo">
-        </a>
-        <div class="heading-text">
-          <span class="company-name">{{ company.name }}</span>
+    <PageNotFound v-if="!showTicker" />
 
-          <a :href="COMPANY_PROFILE_URL + company.id" target="_blank" rel="noopener noreferrer">
-            <span class="company-ticker"> {{ ticker }} </span>
-          </a>
-
+    <div v-if="showTicker">
+      <Heading>
+        <div class="company-title">
+          <img :src="company.logo" :alt="ticker" class="company-logo">
+          <span>{{ ticker }}</span>
+          <span class="company-name"> {{ company.company }} </span>
         </div>
-      </div>
+
+        <div class="company-subtitle">
+          <div class="current-price">
+            <span>{{ currentPrice }}</span>
+            <span :class="numChangeClass(currentChange)" class="current-change">{{ formatNum(currentChange) }}</span>
+          </div>
+
+          <div v-if="company.buyUrl && company.profileUrl" class="company-links">
+            <a :href="company.buyUrl" target="_blank" rel="noopener noreferrer">buy</a>
+            <span class="v-divider"> | </span>
+            <a :href="company.profileUrl" target="_blank" rel="noopener noreferrer">profile</a>
+          </div>
+        </div>
+      </Heading>
+
+      <PeriodNav :period="period" />
+      <Dygraph :data="data" class="price-graph" />
+
       <hr class="divider">
-      <span class="price">{{ currentSummary.curr }}</span>
-      <span :class="numClass(currentSummary.change)" class="num-change">{{ formatNum(currentSummary.change) }}</span>
+
+      <div v-for="tbl in summaryTables" :class="tbl.class" :key="tbl.class">
+        <b-table v-for="keys in tbl.keys" :key="keys[0]" :items="currentItems" :fields="keys.map(key => fields.find(f => f.key === key))" class="summary-table" small stacked>
+          <template slot="change" slot-scope="data">
+            <span :class="numChangeClass(data.value)" class="num-change">{{ formatNum(data.value) }}</span>
+          </template>
+          <template slot="high" slot-scope="data">
+            <span v-b-tooltip.hover :title="formatDate(data)" :class="isCurrentHigh ? 'current-high' : ''" class="num-high">{{ data.value }}</span>
+          </template>
+        </b-table>
+      </div>
     </div>
-    <PeriodNav :period="period" />
-    <Dygraph :data="data" />
   </div>
 </template>
 
-<style scoped>
-.content {
-  margin: 0 5px 10px 5px;
+<style>
+.summary-table.table td:first-child {
+  border: none;
 }
 
-.heading {
-  display: flex;
-  align-items: center;
+.summary-table.table {
+  margin-bottom: 0;
+}
+</style>
+
+<style scoped>
+.company-title {
+  font-size: 1.2rem;
 }
 
 .company-logo {
-  margin-right: 0.75em;
-  width: 2.5rem;
-  height: 2.5rem;
+  margin-right: 0.15em;
+  height: 25px;
 }
 
 .company-name {
-  font-size: 2rem;
-}
-
-.company-ticker {
   margin-left: 0.5em;
-  font-size: 1.15rem;
+  font-size: 0.9rem;
   color: #6c757d;
 }
 
+.company-subtitle {
+  display: flex;
+  justify-content: space-between;
+}
+
+.current-price {
+  font-size: 2.25rem;
+  margin-bottom: -8px;
+}
+
+.current-change {
+  margin-left: 0.2em;
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+
+.company-links {
+  margin-top: auto;
+  white-space: nowrap;
+  font-size: small;
+}
+
+.company-links a {
+  color: #1e7e34;
+}
+
+.v-divider {
+  color: #aaa;
+}
+
 .divider {
-  margin: 5px 0;
+  margin: 15px 0 5px;
 }
 
-.price {
-  font-size: 1.75em;
+@media (max-width: 575.98px) {
+  .price-graph {
+    max-height: 200px;
+  }
 }
 
-.num-change {
-  margin-left: 0.25em;
-  font-size: 1.25rem;
+.summary-tables {
+  display: flex;
 }
 
-.num-positive {
-  color: green;
+.summary-tables-compact {
+  display: none;
 }
 
-.num-negative {
-  color: red;
+@media (max-width: 575.98px) {
+  .summary-tables {
+    display: none;
+  }
+
+  .summary-tables-compact {
+    display: flex;
+  }
+}
+
+.summary-table {
+  flex-basis: 0;
+  flex-grow: 1;
+  white-space: nowrap;
 }
 </style>
 
 <script>
 import Dygraph from './Dygraph'
+import Heading from './Heading'
 import PeriodNav from './PeriodNav'
+import PageNotFound from './PageNotFound'
 
 export default {
   components: {
     Dygraph,
-    PeriodNav
+    Heading,
+    PeriodNav,
+    PageNotFound
   },
 
   props: {
@@ -103,19 +170,56 @@ export default {
   data() {
     return {
       data: null,
-      COMPANY_PROFILE_URL:
-        'http://www.neopets.com/stockmarket.phtml?type=profile&company_id='
+      fields: [
+        {
+          key: 'volume',
+          class: 'numeric',
+          formatter: value => value.toLocaleString()
+        },
+        {
+          key: 'open',
+          class: 'numeric'
+        },
+        {
+          key: 'curr',
+          label: 'Current',
+          class: 'numeric'
+        },
+        {
+          key: 'change',
+          class: 'numeric'
+        },
+        {
+          key: 'high',
+          class: 'numeric'
+        },
+        {
+          key: 'low',
+          class: 'numeric'
+        },
+        {
+          key: 'avg',
+          class: 'numeric'
+        },
+        {
+          key: 'median',
+          class: 'numeric'
+        },
+        {
+          key: 'sd',
+          label: 'Volatility',
+          class: 'numeric'
+        }
+      ]
     }
   },
 
   computed: {
     company() {
-      const data = this.companies[this.ticker]
-      return {
-        name: data.company,
-        id: data.id,
-        logoUrl: '/assets/logos/' + data.logo_id + '.gif'
-      }
+      return this.companies[this.ticker]
+    },
+    showTicker() {
+      return this.company != null
     },
     summary() {
       const periods = Object.keys(this.summaryData)
@@ -125,6 +229,12 @@ export default {
         )
         return data
       }, {})
+    },
+    currentPrice() {
+      return this.summary.period_1d.curr
+    },
+    currentChange() {
+      return this.summary.period_1d.change
     },
     currentSummary() {
       switch (this.period) {
@@ -137,11 +247,35 @@ export default {
         case 'all':
           return this.summary.period_all
       }
+    },
+    currentItems() {
+      return [this.currentSummary]
+    },
+    isCurrentHigh() {
+      return this.currentPrice === this.currentSummary.high && this.currentPrice >= 30
+    },
+    summaryTables() {
+      return [
+        { class: 'summary-tables', keys: this.currentKeys },
+        { class: 'summary-tables-compact', keys: this.currentKeysCompact }
+      ]
+    },
+    currentKeys() {
+      if (this.period === 'all') {
+        return [['median'], ['high', 'low'], ['avg', 'sd']]
+      }
+      return [['volume', 'open', 'change'], ['high', 'low', 'avg'], ['sd', 'median', 'avg']]
+    },
+    currentKeysCompact() {
+      if (this.period === 'all') {
+        return [['median', 'sd'], ['high', 'low', 'avg']]
+      }
+      return [['volume', 'open', 'change', 'sd'], ['high', 'low', 'avg', 'median']]
     }
   },
 
   watch: {
-    id: {
+    ticker: {
       handler() {
         this.updateInputs()
       },
@@ -167,20 +301,28 @@ export default {
 
   methods: {
     updateInputs() {
-      if (!(window.Shiny && window.Shiny.onInputChange)) return
+      if (!window.Shiny || !window.Shiny.onInputChange) return
+
+      // clear data to prevent graph flickering when switching tickers
+      this.data = null
+
       Shiny.onInputChange('stock_price', {
         ticker: this.ticker,
         period: this.period
       })
     },
-    numClass(val) {
+    numChangeClass(val) {
       if (val > 0) return 'num-positive'
       if (val < 0) return 'num-negative'
-      return
+      return 'num-zero'
     },
     formatNum(val) {
-      if (val > 0) return '+' + val
+      if (val >= 0) return '+' + val
       return val
+    },
+    formatDate(val) {
+      const date = new Date(val.item.time_high)
+      return date.toLocaleString() + ' NST'
     }
   }
 }
