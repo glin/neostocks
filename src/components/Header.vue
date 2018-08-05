6c1bd41fef2e1432025260210ea641765641f2e0
Greg Lin <glin@glin.io>
2018-08-04 23:46:54 -0500

    Search bar styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,10 +10,16 @@
           <b-nav-item :to="'/bargain' + query">Bargain</b-nav-item>
           <b-nav-item :to="'/hot' + query">Hot</b-nav-item>
           <b-nav-item :to="'/index' + query">Index</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
         </b-navbar-nav>
 
         <b-navbar-nav class="ml-auto">
-          <b-form-input :formatter="formatSearch" type="text" placeholder="search tickers" @input="onSearch" />
+          <b-nav-form class="search-form">
+            <b-form-input :formatter="formatSearch" class="search-input" type="text" placeholder="search tickers" @input="onSearch" />
+            <b-button class="search-btn" variant="default">
+              <SearchIcon class="search-icon" />
+            </b-button>
+          </b-nav-form>
         </b-navbar-nav>
       </b-collapse>
     </b-container>
@@ -33,10 +39,52 @@
   width: 25px;
   height: 25px;
 }
+
+.search-form {
+  position: relative;
+}
+
+.search-input {
+  padding-right: 50px;
+  min-width: 200px;
+  border-radius: 1px;
+  transition: none;
+}
+
+.search-input:focus {
+  box-shadow: inset 0 0 0 1px rgba(0, 123, 255, 0.25);
+}
+
+.search-btn {
+  position: absolute;
+  right: 0;
+  border: none;
+  background: transparent;
+}
+
+.search-btn:focus,
+.search-btn:hover {
+  box-shadow: none;
+}
+
+.search-icon {
+  display: block;
+  opacity: 0.6;
+}
+
+.search-btn:focus,
+.search-btn:hover > .search-icon {
+  opacity: 1;
+}
 </style>
 
 <script>
+import SearchIcon from '@mdi/svg/svg/magnify.svg'
+
 export default {
+  components: {
+    SearchIcon
+  },
   props: {
     onSearch: {
       type: Function,
