89b4b4eab3af411e3f28f8e22b266c0c18bb503a
Greg Lin <glin@glin.io>
2018-08-10 00:24:36 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+if ! git diff-index --quiet HEAD --; then
+    echo "local changes found. aborting."
+    exit 1
+fi
+
+if [ ! -d "dist" ]; then
+    echo "'dist' directory not found. building."
+    npm run build
+fi
+
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 git branch -D deploy -f > /dev/null 2>&1
 git checkout --orphan deploy
 git reset
-git add dist public -f
+git add dist -f
 git commit -m "Initial commit"
 git push origin deploy -f
 git add .
