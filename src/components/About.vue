5ead27917ae5efe819c7e161b711fc154b8b6e0e
Greg Lin <glin@glin.io>
2018-08-12 23:59:36 -0500

    Update about

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,24 +7,24 @@
       <a href="http://www.neopets.com/stockmarket.phtml" target="_blank" rel="noopener noreferrer">Neopets Stock Market</a>, much like the old Neodaq.com (RIP).
     </p>
 
-    <h5>When does this site update?</h5>
+    <h5>When does the site update?</h5>
     <p>
       Every 30 minutes at :00 and :30.
     </p>
 
     <h5>When does the Neopets Stock Market update?</h5>
     <p>
-      <i>Approximately</i> every 30 minutes, but it's not exact. It's hard to predict what minute stocks will update at a given hour, so prices shown on this site may be a little behind.
+      <i>Approximately</i> every 30 minutes, but it's not exact. It's hard to predict the minute stocks update at a given hour, so data shown here may be a little behind.
     </p>
 
     <h5>Credits</h5>
     <p>
-      All data from 2006 to 2017 originally came from
-      <b>Neodaq.com</b>. It was recovered by
-      <a href="https://www.reddit.com/r/neopets/comments/7rwxwd/neodaqs_data_collected_from_old_neodaqs_website/" target="_blank" rel="noopener noreferrer">/u/HostilePride</a> using WaybackMachine snapshots.
+      All data from 2006 to 2018 originally came from
+      <b>Neodaq.com</b>. Pieces of it were recovered from WaybackMachine snapshots by
+      <a href="https://www.reddit.com/r/neopets/comments/7rwxwd/neodaqs_data_collected_from_old_neodaqs_website/" target="_blank" rel="noopener noreferrer">/u/HostilePride</a>.
     </p>
     <p>
-      All data from Feb to June 2018 was generously contributed by
+      Data from Feb to June 2018 was generously contributed by
       <a href="https://www.reddit.com/user/HostilePride" target="_blank" rel="noopener noreferrer">/u/HostilePride</a>.
     </p>
     <p>
