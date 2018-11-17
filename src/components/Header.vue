fa6aada670edaeeccfd4467c9dfb83b8fc5ecbd6
Greg Lin <glin@glin.io>
2018-11-17 15:40:18 -0600

    Add image alt text

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <b-container>
       <b-navbar-toggle target="nav-menu-collapse" />
       <b-navbar-brand :to="{ path: '/', query }" class="brand">
-        <img class="logo" src="../assets/neostocks.png"> neostocks
+        <img class="logo" src="../assets/neostocks.png" alt="neostocks-logo"> neostocks
       </b-navbar-brand>
       <b-collapse id="nav-menu-collapse" v-model="showNavMenuCollapse" is-nav>
         <b-navbar-nav>
