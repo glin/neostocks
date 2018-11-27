f8a31c7a3845a010530440d071d9293a5f0927a7
Greg Lin <glin@glin.io>
2018-11-26 22:56:34 -0600

    About page layout and font adjustments

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Open+Sans:400');
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600');
 
 html,
 body {
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="about">
-    <h3>About</h3>
-    <hr>
-    <h5>What is this?</h5>
-    <p>A real-time stock tracker for the
-      <a href="http://www.neopets.com/stockmarket.phtml" target="_blank" rel="noopener noreferrer">Neopets Stock Market</a>, similar to the former Neodaq.com (RIP).
-    </p>
+    <h1 class="title">About</h1>
+    <div class="section">
+      <h2 class="section-heading">What is this?</h2>
+      <p>A real-time stock tracker for the
+        <a href="http://www.neopets.com/stockmarket.phtml" target="_blank" rel="noopener noreferrer">Neopets Stock Market</a>, similar to the former Neodaq.com (RIP).
+      </p>
+    </div>
 
-    <h5>When does the Neopets Stock Market update?</h5>
-    <p>
-      About every 30 minutes, but it's not exact. It's hard to predict when stocks will update during a given hour.
-    </p>
+    <div class="section">
+      <h2 class="section-heading">When does the Neopets Stock Market update?</h2>
+      <p>
+        About every 30 minutes, but it's not exact. It's hard to predict when stocks will update during a given hour.
+      </p>
+    </div>
 
-    <h5>Credits</h5>
-    <p>
-      All data from 2006 to 2018 originally came from
-      <b>Neodaq.com</b>. Pieces of it were recovered from WaybackMachine snapshots by
-      <a href="https://www.reddit.com/r/neopets/comments/7rwxwd/neodaqs_data_collected_from_old_neodaqs_website/" target="_blank" rel="noopener noreferrer">/u/HostilePride</a>.
-    </p>
-    <p>
-      Data from Feb to June 2018 was generously contributed by
-      <a href="https://www.reddit.com/user/HostilePride" target="_blank" rel="noopener noreferrer">/u/HostilePride</a>.
-    </p>
-    <p>
-      Thanks!
-    </p>
+    <div class="section">
+      <h2 class="section-heading">Credits</h2>
+      <p>
+        All data from 2006 to 2018 originally came from
+        <strong>Neodaq.com</strong>. Pieces of it were recovered from WaybackMachine snapshots by
+        <a href="https://www.reddit.com/r/neopets/comments/7rwxwd/neodaqs_data_collected_from_old_neodaqs_website/" target="_blank" rel="noopener noreferrer">/u/HostilePride</a>.
+        <br>
+        Data from Feb to June 2018 was generously contributed by
+        <a href="https://www.reddit.com/user/HostilePride" target="_blank" rel="noopener noreferrer">/u/HostilePride</a>.
+        <br>
+        Thanks!
+      </p>
+    </div>
   </div>
 </template>
 
@@ -33,13 +36,23 @@
   padding: 10px 15px 10px;
 }
 
-.about h5 {
-  margin: 25px 0 10px;
-  font-size: 1.15rem;
+.title {
+  margin-bottom: 1.5rem;
+  font-size: 1.25rem;
+  color: #555;
+}
+
+.section:not(:first-of-type) {
+  margin-top: 2rem;
+}
+
+.section-heading {
+  margin-bottom: 5px;
+  font-size: 1.1rem;
   font-weight: 600;
 }
 
-.about p {
-  margin: 0 0 3px;
+.section p {
+  margin-bottom: 5px;
 }
 </style>
