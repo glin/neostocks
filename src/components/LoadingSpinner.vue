85a2eab1ad37a776dc2cf05139c275b8b8be1cbf
Greg Lin <glin@glin.io>
2018-12-30 00:41:20 -0600

    Add loading spinner

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="loader">
+    <clip-loader
+      :color="color"
+      class="loader"
+    />
+  </div>
+</template>
+
+<style scoped>
+.loader {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+<script>
+import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
+
+export default {
+  components: {
+    ClipLoader
+  },
+
+  props: {
+    color: {
+      type: String,
+      default: '#3398db'
+    }
+  }
+}
+</script>
