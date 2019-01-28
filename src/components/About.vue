1bc6d3d249aaac68ade6d547c5d487336b023f38
Greg Lin <glin@glin.io>
2019-01-27 20:25:15 -0600

    Adjust spacing/width on about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,16 +3,17 @@
     <h1 class="title">About</h1>
     <div class="section">
       <h2 class="section-heading">What is this?</h2>
-      <p>A real-time stock tracker for the
-        <a href="http://www.neopets.com/stockmarket.phtml">Neopets Stock Market</a>, similar to the former NeoDaq.com (RIP).
+      <p>
+        A real-time stock tracker for the
+        <a
+          href="http://www.neopets.com/stockmarket.phtml"
+        >Neopets Stock Market</a>, similar to the former NeoDaq.com (RIP).
       </p>
     </div>
 
     <div class="section">
       <h2 class="section-heading">When does the site update?</h2>
-      <p>
-        About every 30 minutes, usually within a minute of Neopets Stock Market updates.
-      </p>
+      <p>About every 30 minutes, usually within a minute of Neopets Stock Market updates.</p>
     </div>
 
     <div class="section">
@@ -20,13 +21,17 @@
       <p>
         All data from 2006 to 2018 originally came from
         <strong>NeoDaq.com</strong>. Pieces of it were recovered from WaybackMachine snapshots by
-        <a href="https://www.reddit.com/r/neopets/comments/7rwxwd/neodaqs_data_collected_from_old_neodaqs_website/">/u/HostilePride</a>.
-        <br>
-        Data from Feb to June 2018 was generously contributed by
-        <a href="https://www.reddit.com/user/HostilePride">/u/HostilePride</a>.
-        <br>
-        Thanks!
+        <a
+          href="https://www.reddit.com/r/neopets/comments/7rwxwd/neodaqs_data_collected_from_old_neodaqs_website/"
+        >/u/HostilePride</a>.
       </p>
+      <p>
+        Data from Feb to June 2018 was generously contributed by
+        <a
+          href="https://www.reddit.com/user/HostilePride"
+        >/u/HostilePride</a>.
+      </p>
+      <p>Thanks!</p>
     </div>
   </div>
 </template>
@@ -34,6 +39,7 @@
 <style scoped>
 .about {
   padding: 20px 25px 20px;
+  max-width: 50em;
 }
 
 .title {
@@ -47,12 +53,13 @@
 }
 
 .section-heading {
-  margin-bottom: 5px;
+  margin-bottom: 0.5rem;
   font-size: 1.1rem;
   font-weight: 600;
 }
 
 .section p {
-  margin-bottom: 5px;
+  margin-bottom: 0.5rem;
+  line-height: 1.7rem;
 }
 </style>
