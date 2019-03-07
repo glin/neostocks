93d5888c021e658305822df91981700dbcbfb4b0
Greg Lin <glin@glin.io>
2019-03-06 22:24:57 -0600

    Test that HTML pages are generated correctly

--- tests/testthat/test-ui.R
+++ tests/testthat/test-ui.R
@@ -18,6 +18,54 @@ test_that("index_page", {
   ))
 })
 
+test_that("ui_template", {
+  template_file <- "data/template.html"
+
+  # Summary only
+  stock_data <- list(market_summary = list(price = 5), price_data = NULL)
+  html <- ui_template(template_file, stock_data, title = "summary")
+  expected <- '<html>
+<head>
+<meta property="og:title" content="summary - neostocks"/>
+<title>summary - neostocks</title>
+</head>
+<body>
+<script>window.__data__ = {"price":5}</script>
+</body>
+</html>'
+  expect_equal(as.character(html), expected)
+
+  # With ticker prices
+  stock_data <- list(
+    market_summary = list(price = 5),
+    price_data = list(
+      "1d" = data.table(
+        ticker = c("AAVL"),
+        time = c(100),
+        curr = c(22)
+      )
+    )
+  )
+  html <- ui_template(
+    template_file,
+    stock_data,
+    title = "ticker",
+    ticker = "AAVL",
+    period = "1d"
+  )
+  expected <- '<html>
+<head>
+<meta property="og:title" content="ticker - neostocks"/>
+<title>ticker - neostocks</title>
+</head>
+<body>
+<script>window.__data__ = {"price":5}</script>
+<script>window.__ticker_data__ = {"prices":{"time":[100],"curr":[22]},"ticker":"AAVL","period":"1d"}</script>
+</body>
+</html>'
+  expect_equal(as.character(html), expected)
+})
+
 test_that("document_title", {
   expect_equal(document_title(), "neostocks")
   expect_equal(document_title("mypage"), "mypage - neostocks")
