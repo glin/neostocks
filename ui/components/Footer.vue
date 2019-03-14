8239ff2129aa5b0f43e9fa1db1b72f0a1d064c8c
Greg Lin <glin@glin.io>
2019-03-13 22:05:26 -0500

    Improve readability of footer text

--- ui/components/Footer.vue
+++ ui/components/Footer.vue
@@ -31,11 +31,11 @@ export default {
   text-align: center;
   padding: 1.25rem 1rem;
   font-size: 0.75rem;
-  color: hsl(0, 0%, 60%);
+  color: hsl(0, 0%, 55%);
 }
 
 .footer-link {
-  color: hsl(0, 0%, 60%);
+  color: hsl(0, 0%, 55%);
 }
 
 .spacer {
