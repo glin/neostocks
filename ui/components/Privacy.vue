426dd59972ae24e2086590cfb31c240b8cb89188
Greg Lin <glin@glin.io>
2019-03-19 23:40:13 -0500

    Fix title caps

--- ui/components/Privacy.vue
+++ ui/components/Privacy.vue
@@ -10,7 +10,7 @@
 
     <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from neostocks.info.</p>
 
-    <h2>Information Collection And Use</h2>
+    <h2>Information Collection and Use</h2>
 
     <p>We collect several different types of information for various purposes to provide and improve our Service to you.</p>
 
@@ -54,13 +54,13 @@
       <li>To detect, prevent and address technical issues</li>
     </ul>
 
-    <h2>Transfer Of Data</h2>
+    <h2>Transfer of Data</h2>
     <p>Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.</p>
     <p>If you are located outside United States and choose to provide information to us, please note that we transfer the data, including Personal Data, to United States and process it there.</p>
     <p>Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.</p>
     <p>neostocks will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.</p>
 
-    <h2>Disclosure Of Data</h2>
+    <h2>Disclosure of Data</h2>
 
     <h3>Legal Requirements</h3>
     <p>neostocks may disclose your Personal Data in the good faith belief that such action is necessary to:</p>
@@ -72,7 +72,7 @@
       <li>To protect against legal liability</li>
     </ul>
 
-    <h2>Security Of Data</h2>
+    <h2>Security of Data</h2>
     <p>The security of your data is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.</p>
 
     <h2>Service Providers</h2>
@@ -97,7 +97,7 @@
       </li>
     </ul>
 
-    <h2>Links To Other Sites</h2>
+    <h2>Links to Other Sites</h2>
     <p>Our Service may contain links to other sites that are not operated by us. If you click on a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
     <p>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
 
@@ -105,7 +105,7 @@
     <p>Our Service does not address anyone under the age of 18 ("Children").</p>
     <p>We do not knowingly collect personally identifiable information from anyone under the age of 18. If you are a parent or guardian and you are aware that your Children has provided us with Personal Data, please contact us. If we become aware that we have collected Personal Data from children without verification of parental consent, we take steps to remove that information from our servers.</p>
 
-    <h2>Changes To This Privacy Policy</h2>
+    <h2>Changes to this Privacy Policy</h2>
     <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
     <p>We will let you know via a prominent notice on our Service, prior to the change becoming effective and update the "effective date" at the top of this Privacy Policy.</p>
     <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
